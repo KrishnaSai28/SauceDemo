f81dc7c64337aec44289bde6435bb340e009a0e8
Krishna <Krishnasai2801@gmail.com>
2024-07-26 08:18:57 +0530

    first commit

---
+++ GitStuff/src/selenium/complete.java
@@ -0,0 +1,14 @@
+package selenium;
+
+public class complete {
+	
+	public void rest()
+	{
+		System.out.println("First");
+		System.out.println("Second");
+		
+		System.out.println("Third");
+		System.out.println("Fourth");
+	}
+
+}
